b80358b14045ad1b5d32af365a1bbacd3f6f6983
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-01 15:06:11 +0000

    Uploading changes to hello.js and new file even.js to show that functions can be called directly inside console.log. Practice exercise!

--- hello.js
+++ hello.js
@@ -20,3 +20,27 @@ sayHello("Miranda");
 sayHello("Ferdinand");
 
 */
+
+// Example 3: console.log
+/* const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+*/
+
+
+// Example 4: return
+
+/*const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+*(/)
+
+// Example: console.log + return
+/*const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+*/
