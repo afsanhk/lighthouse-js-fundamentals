6e73c5204696da69aff686f94efc2bb208e66e9d
Afsanul Haque Khan <afsanhk@gmail.com>
2021-06-21 21:39:10 +0000

    Refactored loopyLighthouse2.js to have fewer if statements

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -9,3 +9,20 @@ for (let i = 100; i <= 200; i++) {
     console.log(i);
   }
 }
+
+
+/* REFACTORED
+
+for (const num of nums) {
+  let output = "";
+
+  if (num % 3 === 0) {
+    output += "Loopy";
+  }
+  if (num % 4 === 0) {
+    output += "Lighthouse";
+  }
+  console.log(output === "" ? num : output);
+}
+
+*/ 
--- loopyLighthouse2.js
+++ loopyLighthouse2.js
@@ -17,5 +17,29 @@ function loopyLighthouse(range, multiples, words) {
 
 }
 
+
+// REFACTORED
+
+/*
+function loopyLighthouse(range, multiples, words) {
+  
+  let start = range[0];
+  let end = range[1];
+
+  for (let i = start; i <= end; i++) {
+    let output = '';
+
+    if(i % multiples[0] === 0) {
+      output += words[0];
+    }
+    if(i % multiples[1] === 0) {
+      output += words[1];
+    }
+    console.log(output === '' ? i : output);
+  }
+
+}
+*/
+
 // Test with
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
